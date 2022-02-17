e93709abb05148a793074570d30ddfdd460ed29f
Lakshay Sachdeva <lakshay07sachdeva@gmail.com>
2022-02-18 03:25:19 +0530

    login component done, header component under progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import { Login } from "./components/Login";
+import { Header } from "./components/Header";
 import "./App.css";
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Login />}></Route>
-    </Routes>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+      </Routes>
+    </div>
   );
 };
