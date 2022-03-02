91951bb27203353eff3ea8e89f91e7a76561e3b4
Lakshay Sachdeva <lakshay07sachdeva@gmail.com>
2022-03-03 04:16:28 +0530

    home page components in progress

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,33 @@
 import styled from "styled-components";
 import { ImageSlider } from "./ImageSlider";
+import { Viewers } from "./Viewers";
+import { Recommends } from "./Recommends";
 
 export const Home = () => {
   return (
     <Container>
       <ImageSlider />
+      <Viewers />
+      <Recommends />
     </Container>
   );
 };
 
 const Container = styled.main`
-position:relative ;
-min-height: calc(100vh - 250px) ;
-overflow-x: hidden;
-display: block;
-top: 72px;
-padding: calc(3.5vw + 5px);
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: calc(3.5vw + 5px);
 
-&:after{
-    background: url("/images/home-background.png") center / cover no-repeat fixed;
+  &:after {
+    background: url("/images/home-background.png") center / cover no-repeat
+      fixed;
     content: "";
     position: absolute;
     inset: 0px;
     opacity: 1;
     z-index: -1;
-}
+  }
 `;
